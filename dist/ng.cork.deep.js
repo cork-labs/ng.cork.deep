de1ec3819ed1bae9c82eafa11f58594dbdd895bb
Andre Torgal <andre.torgal@gmail.com>
2015-04-16 22:49:05 +0100

    chore(release): v0.0.5

--- dist/ng.cork.deep.js
+++ dist/ng.cork.deep.js
@@ -1,5 +1,5 @@
 /**
- * ng.cork.deep - v0.0.4 - 2015-04-09
+ * ng.cork.deep - v0.0.5 - 2015-04-16
  * https://github.com/cork-labs/ng.cork.deep
  *
  * Copyright (c) 2015 Cork Labs <http://cork-labs.org>
